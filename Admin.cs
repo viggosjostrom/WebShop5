cbe5dd23b9eea92a234bc6df97851f9fc4b803dd
stoffeek <stoffeek@gmail.com>
2023-10-06 00:04:53 +0200

    Skapade inloggning för admin och modifiera Program.cs

--- Admin.cs
+++ Admin.cs
@@ -4,17 +4,40 @@ namespace WebShop5
     public class Admin
     {
     
-        static public void AdminCheck()
+        static public void AdminLogIn()
         {
 
-           bool IsAdmin = true;
+            string AdminUserName = "Admin";
+            string AdminPassword = "WebShop5";
 
-          while (IsAdmin)
-          {
-            Console.WriteLine("Admin Username :");
-            Console.WriteLine("Password :");
-            Console.ReadLine();
-          }
+            Console.WriteLine("Welcome to admin log in!");
+
+            Console.WriteLine("Enter your admin log in: ");
+            string inputLogin = Console.ReadLine();
+            Console.WriteLine("Enter your password: ");
+            string inputPassword = Console.ReadLine();
+
+            if(inputLogin == AdminUserName && inputPassword == AdminPassword)
+            {
+                bool AdminMenu = true;
+                while (AdminMenu)
+                {
+                    Console.WriteLine("ADMIN MENY: ");
+                    Console.WriteLine("0 = EXIT");
+                    int AdminChoice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (AdminChoice)
+                    {
+                        case 0: AdminMenu = false;
+                            break;
+
+                    }
+
+                }
+
+            }
+
+
 
 
         }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             bool Showmenu = true;
 
@@ -12,16 +12,19 @@
                 Console.WriteLine("1 - Skapa en användare");
                 Console.WriteLine("2 - Logga in som användare");
                 Console.WriteLine("3 - Logga in som admin");
+                Console.WriteLine("0 - To Exit");
                 int Choice = Convert.ToInt32(Console.ReadLine());
                 switch (Choice)
                 {
+                    case 0: break;
+
                   //*  case "1": User();
                   //*      break;
                     
                   //*  case "2": LogInUser();
                   //*      break;
 
-                    case 3: Admin.AdminCheck();
+                    case 3: Admin.AdminLogIn();
                         break;
                 }
 
