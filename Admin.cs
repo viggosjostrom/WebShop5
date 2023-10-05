b56677cf0adcf769cde444fb094d1668e204aa02
stoffeek <stoffeek@gmail.com>
2023-10-05 16:15:25 +0200

    Skapa meny/börjat på admin

--- Admin.cs
+++ Admin.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebShop5;
-
-public class Admin : User
+﻿
+namespace WebShop5
 {
+    public class Admin
+    {
+    
+        static public void AdminCheck()
+        {
+
+           bool IsAdmin = true;
+
+          while (IsAdmin)
+          {
+            Console.WriteLine("Admin Username :");
+            Console.WriteLine("Password :");
+            Console.ReadLine();
+          }
+
+
+        }
+
+        
+        
+      
+    }   
+
 }
+
+
--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿namespace WebShop5
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool Showmenu = true;
+
+            while (Showmenu)
+            {
+                
+                Console.WriteLine("1 - Skapa en användare");
+                Console.WriteLine("2 - Logga in som användare");
+                Console.WriteLine("3 - Logga in som admin");
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                  //*  case "1": User();
+                  //*      break;
+                    
+                  //*  case "2": LogInUser();
+                  //*      break;
+
+                    case 3: Admin.AdminCheck();
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
