41a90ef1d9894ad05aafc63eb3bf39b0252eadce
Clone888 <dwclone@gmail.com>
2023-10-08 11:42:29 +0200

    Added function to countinue adding products

--- Product.cs
+++ Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,23 +17,37 @@ public class Product
     public static void ProductInventory()
     {
 
-        List<string> productList = new List<string> (File.ReadAllLines("../../../products.csv"));
+        List<string> productList = new List<string>(File.ReadAllLines("../../../products.csv"));
 
 
         List<string> productName = new List<string>();
         List<string> productPrice = new List<string>();
 
-        Console.WriteLine("Add a product");
-        string? addProduct = Console.ReadLine();
-        productList.Add(addProduct);
-        File.WriteAllLines("../../../products.csv", productList);
-
-        Console.WriteLine("Add price for the product");
-        string addPrice = Console.ReadLine();
-        productList.Add(addPrice);
-        File.WriteAllLines("../../../products.csv", productList);
+        string newProduct = string.Empty;
 
 
+
+        while (newProduct != "n")
+        {
+
+            Console.WriteLine("\t\t\tADD PRODUCTS\n");
+
+            Console.Write("Add a product: ");
+            string? addProduct = Console.ReadLine();
+            productList.Add(addProduct);
+            File.WriteAllLines("../../../products.csv", productList);
+
+            Console.Write("Add price for the product: ");
+            string addPrice = Console.ReadLine();
+            productList.Add(addPrice + "$");
+            File.WriteAllLines("../../../products.csv", productList);
+            Console.Clear();
+
+            Console.Write("Want to add more? y/n?: ");
+            newProduct = Console.ReadLine().ToLower();
+            Console.Clear();
+
+        }
 
         foreach (string row in productList)
         {
