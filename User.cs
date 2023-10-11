7e047abbfd5517198cc0480d469d42245927f6ff
viggosjostrom <viggosjostrom1@gmail.com>
2023-10-11 10:40:50 +0200

    uppdaterat UserMenu för Go to payment metod

--- User.cs
+++ User.cs
@@ -142,7 +142,8 @@ public class User
             Console.WriteLine("1 : Show Products");
             Console.WriteLine("2 : Shop Products");
             Console.WriteLine("3 : Remove from your Shopping Bag");
-            Console.WriteLine("4 : Order History");
+            Console.WriteLine("4 : Go to payment / Checkout");
+            Console.WriteLine("5 : Order History");
             Console.WriteLine("0 : Sign Out");
             int UserChoice = Convert.ToInt32(Console.ReadLine());
 
@@ -171,8 +172,12 @@ public class User
                     // Användaren kan ta bort produkter från sin shoppingbag
                     Products.RemoveFromShoppingbag();
                     break;
-                
                 case 4:
+                    // Användaren kan efter att ha lagt till produkter i sin varukorg nu välja
+                    // att genomföra köpet. Denna infon ska kopplas till kvitto / orderhistorik för användaren
+                    // Products.PurchaseShoppingBag();
+                    break;
+                case 5:
                     // Order history (Fredriks kod)
                     break;
 
